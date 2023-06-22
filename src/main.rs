bcc1cd6c443ed528e479fe2f3f616671fef30920
Cristian Urlea <cristian.urlea@glasgow.ac.uk>
2023-06-22 17:55:26 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::ptr;
 use morello::capability::*;
 use cheribsd_capability_rs::cheribsd::get_root_seal;
 
-fn print_cap(cap: Capability, name: &str) {
+fn print_cap(cap: *const i64, name: &str) {
 
     // Extract the address portion of &arr
     let addr1_usize : usize = get_address(&cap);
@@ -16,17 +16,17 @@ fn print_cap(cap: Capability, name: &str) {
 }
 
 fn main() {
-    
-    let root_seal: *mut Capability = ptr::null_mut() as *mut Capability;
-    let size = std::mem::size_of_val(&root_seal);
+    let mut value : i64 = 0;
 
+    let root_seal: *const i64 = &value;
+    let size = std::mem::size_of_val(&root_seal);
     let result = get_root_seal(root_seal, size);
 
     match result {
         Ok(_value) => {
 
-            let cap_val = unsafe { *root_seal };
-            print_cap(cap_val , "example");
+            // let cap_val = unsafe { *root_seal };
+            print_cap(root_seal , "example");
 
         }
         Err(error) => {
