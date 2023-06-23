f29658c88589ac6d35867c225bf87fe0a244e5ff
Cristian Urlea <cristian.urlea@glasgow.ac.uk>
2023-06-23 11:37:51 +0100

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::ptr;
+
 use morello::capability::*;
 use cheribsd_capability_rs::cheribsd::get_root_seal;
 
 fn print_cap(cap: *const i64, name: &str) {
 
     // Extract the address portion of &arr
-    let addr1_usize : usize = get_address(&cap);
-
-    println!("&arr  address: {},   bounds: {}, for {}", addr1_usize, get_length(&cap), name);
+    let addr1_usize : usize = get_address(cap);
+    let perms = get_perms(cap);
+    println!("&arr  address: {},   bounds: {}, for {}, {} ", addr1_usize, get_length(&cap), name, perms);
 
     // unsafe {
     //     // Dereference our restricted capability
@@ -18,19 +18,29 @@ fn print_cap(cap: *const i64, name: &str) {
 fn main() {
     let mut value : i64 = 0;
 
-    let root_seal: *const i64 = &value;
-    let size = std::mem::size_of_val(&root_seal);
-    let result = get_root_seal(root_seal, size);
+ //   unsafe  {
+    let  mut root_seal = &mut value as *mut i64;
+
+    print_cap(root_seal, "asdf");
+
+    let mut size = std::mem::size_of_val(&root_seal);
+    let result = get_root_seal(&mut root_seal, &mut size as *mut usize);
 
     match result {
         Ok(_value) => {
+            println!("ok");
 
-            // let cap_val = unsafe { *root_seal };
-            print_cap(root_seal , "example");
 
-        }
+            print_cap(root_seal, "done");
+       }
         Err(error) => {
             println!("Error: {}", error);
         }
-    }}
+    }
 
+
+   // }
+
+
+
+}
