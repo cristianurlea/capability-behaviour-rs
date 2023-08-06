70de9603c34acace75ae385769bea07307e698c9
Cristian Urlea <cristian.urlea@glasgow.ac.uk>
2023-08-06 22:25:27 +0100

    macros and opaques

--- generate/src/lib.rs
+++ generate/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
-use quote::ToTokens;
+use quote::{ToTokens,quote};
 
 #[proc_macro_attribute]
 pub fn print_ast(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -16,4 +16,38 @@ pub fn print_ast(_args: TokenStream, input: TokenStream) -> TokenStream {
 
 
     input.into_token_stream().into()
+}
+
+#[proc_macro_derive(HelloMacro)]
+pub fn derive_impl(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+// fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+//     let name = &ast.ident;
+//     let gen = quote! {
+//         impl HelloMacro for #name {
+//             fn hello_macro() {
+//                 println!("Hello, Macro! My name is {}!", stringify!(#name));
+//             }
+//         }
+//     };
+//     gen.into()
+// }
+
+fn impl_hello_macro<'a>(ast: &'a syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl<'a> HelloMacro for #name<'a> {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
 use capability_behaviour_rs::compartments::with_new_stack;
-use capability_generate::{print_ast};
+use capability_generate::{print_ast,HelloMacro};
+use std::collections::HashSet;
+use std::sync::{Mutex, Arc};
+use lazy_static::{lazy_static};
 
 // usual perms :: field in abstract cap 
 // revokable permission :: field in abstract cap
@@ -10,12 +13,47 @@ use capability_generate::{print_ast};
 //  -- ip limiting example 
 
 
+mod my_opaque {
+    use super::*;
+
+    #[derive(Debug)]
+    pub struct Opaque(i32);
+
+    
+    impl Opaque {
+        pub fn new(value: i32) -> Opaque {
+            // Perform any validation or preprocessing here.
+
+            register_value(value);
+
+            Opaque(value)
+        }
+    }
+
+}
+
+lazy_static! {
+    // The global thread-safe HashSet to store the constructed values.
+    static ref CONSTRUCTED_VALUES: Mutex<HashSet<i32>> = Mutex::new(HashSet::new());
+}
+
+fn register_value(value: i32) {
+    let mut set = CONSTRUCTED_VALUES.lock().unwrap();
+    set.insert(value);
+}
+
+
+pub trait HelloMacro {
+    fn hello_macro();
+}
+
 #[print_ast]
-struct Alpha<'a,'b> {
+#[derive(HelloMacro)]
+struct Alpha<'a> {
     value: &'a str,
-    value2: &'b str,
     b: u8,
 }
+
 
 fn main() {
     let my_string = String::from("Hello, lifetimes!");
@@ -23,5 +61,20 @@ fn main() {
 
     println!("ok");
     with_new_stack();
-    println!("ok doky {}", Alpha {  value: &my_string ,value2: &my_string,  b: 3 }.b);
+    println!("ok doky {}", Alpha { value: &my_string , b: 3 }.value);
+
+
+    let instance = my_opaque::Opaque::new(42);
+    println!("Constructed opaque instance: {:?}", instance);
+
+    let instance2 = my_opaque::Opaque::new(42);
+    println!("Constructed opaque instance: {:?}", instance2);
+
+    // should fail 
+    // println!("Constructed opaque instance: {:?}", my_opaque::Opaque(33));
+
+        // Print the stored values
+        let set = CONSTRUCTED_VALUES.lock().unwrap();
+        println!("Stored values: {:?}", *set);
+
 }
